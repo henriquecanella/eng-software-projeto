bb3fa2f9e5208901b2481bc12118f4d2fcd66cfa
henriquecanella <henrique.mandelli@gmail.com>
2024-01-10 08:56:40 -0300

    update Dockerfile and docker-compose to use poetry and start fastapi

--- fruit_test.py
+++ fruit_test.py
@@ -11,11 +11,11 @@ def fruit_basket_ok(my_fruit):
     return [Fruit("banana"), my_fruit]
 
 @pytest.fixture()
-def fruit_basket_failure(my_fruit):
+def fruit_basket_failure():
     return [Fruit("banana")]
 
 def teste_my_fruit_in_basket(my_fruit, fruit_basket_ok):
     assert my_fruit in fruit_basket_ok
 
-def teste_my_fruit_in_basket(my_fruit, fruit_basket_failure):
-    assert my_fruit in fruit_basket_failure
+def teste_my_fruit_in_basket_failure(my_fruit, fruit_basket_failure):
+    assert my_fruit not in fruit_basket_failure
